d1437c0d31c489e87b3aff268e43245f31d5cf99
Paul-Emmanuel <63041127+pechouc@users.noreply.github.com>
2020-10-10 15:41:51 +0200

    Adding all the .py files and the cameras.csv file in data/ (#2)
    * Adding all the .py files and the cameras.csv file in data/
    
    * Updating requirements.txt and correcting based on Codacy
    
    * Correcting additional Codacy errors and adding departments.geojson file
    
    * Still trying to make Codacy corrections + killing urllib in the requirements
    
    * Mistakenly erased the import json line
    
    * Trying with quotation marks
    
    * Getting crazy with the formatting of the docstrings

---
+++ pyronear-webapp/navbar.py
@@ -0,0 +1,37 @@
+"""The following is dedicated to the navigation bar at the top of the web application.
+
+Its main component is the Navbar function that instantiates the navigation bar.
+"""
+
+# ------------------------------------------------------------------------------
+# Imports
+
+import dash_bootstrap_components as dbc
+
+
+# ------------------------------------------------------------------------------
+# Content
+
+def Navbar():
+
+    navbar = dbc.NavbarSimple(
+                              children = [
+                                          dbc.DropdownMenu(
+                                                           nav = True,
+                                                           in_navbar = True,
+                                                           label = 'Tableaux de bord',
+                                                           children = [
+                                                                       dbc.DropdownMenuItem('Alertes et Infrastructures',
+                                                                                            href = 'alerts'),
+
+                                                                       dbc.DropdownMenuItem('Niveaux de Risque',
+                                                                                            href = 'risks')
+                                                                       ],
+                                                            ),
+                                          ],
+                              brand = 'À propos',
+                              brand_href = '/home',
+                              sticky = 'top'
+                              )
+
+    return navbar
