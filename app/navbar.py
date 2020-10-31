f5f2a2a14d28d1940e5d40de94ea786d863c18d4
Akilditu <sebastien.elmaleh@digitalinkers.com>
2020-10-31 19:19:38 +0100

    feat: designing a new navbar
    incorporating pyronear logo, black colored background, new cta style and a centered text retreiving username logged

--- app/navbar.py
+++ app/navbar.py
@@ -7,24 +7,58 @@ Its main component is the Navbar function that instantiates the navigation bar.
 # Imports
 
 import dash_bootstrap_components as dbc
-
+import dash_html_components as html
+from pathlib import Path
+import base64
 
 # ------------------------------------------------------------------------------
 # Content
 
+
+# Encoding logo image file
+pyro_logo = Path(__file__).parent.joinpath('data', 'pyro_logo.png')
+encoded_image = base64.b64encode(open(pyro_logo, 'rb').read())
+
+
 def Navbar():
 
-    navbar = dbc.NavbarSimple(
+    # Dropdown menu
+    dropdown = dbc.DropdownMenu(
         children=[
-            dbc.DropdownMenu(nav=True,
-                             in_navbar=True,
-                             label='Tableaux de bord',
-                             children=[
-                                 dbc.DropdownMenuItem('Alertes et Infrastructures', href='alerts'),
-                                 dbc.DropdownMenuItem('Niveaux de Risque', href='risks')],
-                             )],
-        brand='À propos',
-        brand_href='/home',
-        sticky='top')
+            dbc.DropdownMenuItem('Alertes et Infrastructures', href='alerts'),
+            dbc.DropdownMenuItem('Niveaux de Risque', href='risks'),
+        ],
+        label="Tableaux de bord",
+        className="ml-auto flex-nowrap mt-3 mt-md-0 btn-group dropleft",
+        color="warning",
+        direction="left",
+    )
+
+    # Logged user info
+    user_item = html.Div(
+        "SDIS PyroDev",
+        id="user-div",
+        className="ml-auto flex-nowrap mt-3 mt-md-0",
+        tyle={'color': 'white'})
+
+    # Navbar
+    navbar = dbc.Navbar(
+        [
+            html.A(
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), width="120px")),
+                    ],
+                    align="center",
+                    no_gutters=True,
+                ),
+                href="http://pyronear.org/",
+            ),
+            dbc.NavbarToggler(id="navbar-toggler"),
+            dbc.Collapse([user_item, dropdown], id="navbar-collapse", navbar=True),
+        ],
+        color="black",
+        dark=True,
+    )
 
     return navbar
