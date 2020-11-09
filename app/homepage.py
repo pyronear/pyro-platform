d5c27ee384d8be67737ec04b23a9df6167383a50
VincGargasson <Vincennt@live.fr>
2020-11-09 19:13:00 +0100

    Marker popup associated with click feature and video display function

--- app/homepage.py
+++ app/homepage.py
@@ -57,6 +57,60 @@ def choose_map_style(n_clicks):
     return button_content_map, map_object, slider
 
 
+#This function return the user selection area in the left side bar
+def user_selection_area():
+    """ This function return the basic dash component of the the left side bar
+    of the monitoring platform and has been create because it used several time in app.
+
+    Parameters:
+    None
+
+    Returns:
+    html dash components: A set of component that composed the left side bar of the app
+    """
+    return [dcc.Markdown('---'),
+            html.H5(("Filtres Carte"), style={'text-align': 'center'}),  # map filters added here
+            html.P(map_layers_button),
+            dcc.Markdown('---'),
+            html.P(map_style_button),
+            html.P(id="hp_slider"),
+            html.P(id="hp_video")
+            ]
+
+
+# This function either displays video of selected markers
+def show_camera_video(feature=None):
+    """ This function return the video of the marker and put it into the left side bar
+    of the monitoring platform
+
+    Parameters:
+    Feature (geojson children): Number of click the marker has received
+
+    Returns:
+    html dash components: A default video for now (later, the real video / picture of the marker)
+    """
+
+    video_style = {'width': '50vh',
+                   'height': '40vh',
+                   'margin': 'auto',
+                   'display': 'block'
+                   }
+
+    if feature is not None:
+        separator = dcc.Markdown('---')
+        video_title = html.H5("Camera selectionnée", style={'text-align': 'center'})
+        video_html = html.Video(id='Video_Camera',
+                                src='https://media.giphy.com/media/l2JeaPjeaR9DvDWXS/giphy.mp4',
+                                style=video_style,
+                                controls=True)
+
+        video_div = html.Div(style=video_style,
+                             children=[separator, video_title, video_html]
+                             )
+        return html.Center(video_div)
+    return ' '
+
+
 # This function either displays or hides meteo graphs from graphs.py
 def meteo_graphs(display=False):
     if display is True:
@@ -96,6 +150,7 @@ map_style_button = build_map_style_button()
 # Instantiating meteo graphs, set to True to display them under the map, False to hide them
 meteo_graphs = meteo_graphs(display=False)
 
+
 # Body container
 body = dbc.Container([
     dbc.Row(
@@ -116,15 +171,9 @@ body = dbc.Container([
     ),
     dbc.Row(
         [dbc.Col(
-            [
-                dcc.Markdown('---'),
-                # map filters added here
-                html.H5(("Filtres Carte"), style={'text-align': 'center'}),
-                html.P(map_layers_button),
-                dcc.Markdown('---'),
-                html.P(map_style_button),
-                html.P(id="hp_slider"),
-            ],
+            #side bar for the user to apply filter
+            user_selection_area(),
+            id='user_selection_column',
             md=3),
          dbc.Col(
             # map object added here
