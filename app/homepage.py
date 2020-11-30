fbd63087a6e74cfdb27c969c208909730d1cca1e
nicolas rousselet <64283717+nrslt@users.noreply.github.com>
2020-11-30 16:24:04 +0100

    Update homepage.py

--- app/homepage.py
+++ app/homepage.py
@@ -52,7 +52,7 @@ def build_alert_radio_button():
     return alert_radio_button
 
 
-# The following block is used to determine what map styles (risks or alerts) we use for and enable the user to change it
+# The following block is used to determine which map styles (risks or alerts) we use and enable the user to change it
 # This function creates the button that allows users to change the map style
 def build_map_style_button():
 
