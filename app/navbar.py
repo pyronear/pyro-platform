5c757588ef2a22b9a4681bb4069648ccae7576a5
F-G Fernandez <fgfm03@hotmail.fr>
2021-03-03 22:43:05 +0100

    chore: Updated license from GPLv3 to Apache 2.0 (#40)
    * chore: Switched from GPLv3 to Apache 2.0
    
    * docs: Updated README reference to license
    
    * chore: Added copyright and license notices
    
    * test: Added unittest for header check
    
    * chore: Added CI job to check headers

--- app/navbar.py
+++ app/navbar.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2021, Pyronear contributors.
+
+# This program is licensed under the Apache License version 2.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0.txt> for full license details.
+
 """
 The following file is dedicated to the navigation bar at the top of the web application.
 
