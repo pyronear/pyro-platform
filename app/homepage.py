274dd81c59b4d9ce9dc3b48768b58aac840a5ce8
Sebastien Elmaleh <sebastien.elmaleh@digitalinkers.com>
2021-04-27 21:42:54 +0200

    web socket v6

--- app/homepage.py
+++ app/homepage.py
@@ -359,7 +359,7 @@ def Homepage():
                     html.Div(id='current_map_style', children='alerts', style={'display': 'none'}),
                     # Hidden div storing the webscocket message sent by the API
                     html.Div(id="msg", style={'display': 'none'}),
-                    WebSocket(id="ws"),
+                    WebSocket(url="wss://pyro-platform.herokuapp.com/wss", id="ws"),
                 ],
                     id='map_column',
                     md=12),
