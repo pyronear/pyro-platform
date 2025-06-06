8f6da7c17e588e1fd19c4de3fdf2bc929d71a721
MateoLOSTANLEN <mateo.lostanlen@gmail.com>
2025-06-06 15:53:46 +0200

    fix indent

--- app/callbacks/display_callbacks.py
+++ app/callbacks/display_callbacks.py
@@ -576,15 +576,17 @@ def blink_image(n_intervals, api_sequences):
         # Get current UTC time
         now_utc = datetime.now(timezone.utc)
 
+        print(api_sequences["last_seen_at"], now_utc)
+
         # Find sequences where last_seen_at is within the last 15 minutes
-        recent_sequences = api_sequences[api_sequences["last_seen_at"] > now_utc - timedelta(minutes=15)]
+        recent_sequences = api_sequences[api_sequences["last_seen_at"] > now_utc - timedelta(minutes=30)]
 
         if recent_sequences.empty:
             image_path = "https://pyronear.org/img/logo_letters_orange.png"
             container_style["background-color"] = "#044448"
         else:
             image_path = "https://pyronear.org/img/logo_letters_orange.png"
-        container_style["background-color"] = "red" if n_intervals % 2 == 0 else "#044448"
+            container_style["background-color"] = "red" if n_intervals % 2 == 0 else "#044448"
 
     return [image_path, container_style]
 
