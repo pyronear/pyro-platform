cfe65ab7bcb08a3f2823c6ecd9faa587df980652
MateoLOSTANLEN <mateo.lostanlen@gmail.com>
2025-07-17 18:52:52 +0200

    get cam info

--- app/callbacks/export_callbacks.py
+++ app/callbacks/export_callbacks.py
@@ -3,6 +3,8 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
+from io import StringIO
+
 import logging_config
 import pandas as pd
 from dash import Input, Output, State, dcc
@@ -36,13 +38,22 @@ def trigger_export(n_clicks, lang):
     State("export-end-date", "date"),
     State("user_token", "data"),
     State("language", "data"),
+    State("api_cameras", "data"),
     prevent_initial_call=True,
 )
-def handle_export(trigger, start_date, end_date, user_token, lang):
+def handle_export(trigger, start_date, end_date, user_token, lang, api_cameras):
     if not trigger or not user_token or not start_date or not end_date:
         raise PreventUpdate
 
+    try:
+        cameras_df = pd.read_json(StringIO(api_cameras), orient="split")
+        cameras_df = cameras_df.rename(columns={"id": "camera_id_metadata"})
+        cameras_df = cameras_df[["camera_id_metadata", "name", "angle_of_view", "lat", "lon"]]
+    except Exception as e:
+        return None, f"Erreur lecture des caméras : {e}"
+
     client = get_client(user_token)
+
     try:
         all_sequences = []
         current = pd.to_datetime(start_date)
@@ -56,13 +67,36 @@ def handle_export(trigger, start_date, end_date, user_token, lang):
             current += pd.Timedelta(days=1)
 
         if not all_sequences:
-            return None, translate("no_data_found", lang), False
+            return None, translate("no_data_found", lang)
 
         export_df = pd.concat(all_sequences, ignore_index=True)
-        return (dcc.send_data_frame(export_df.to_csv, "export.csv", index=False), translate("download_ready", lang))
+
+        if "camera_id" in export_df.columns:
+            export_df = export_df.merge(cameras_df, how="left", left_on="camera_id", right_on="camera_id_metadata")
+            export_df = export_df.drop(columns=["camera_id", "camera_id_metadata"])
+        else:
+            return None, "camera_id column missing", False
+
+        # Reorder columns
+        ordered_columns = [
+            "id",
+            "name",
+            "azimuth",
+            "cone_angle",
+            "started_at",
+            "last_seen_at",
+            "is_wildfire",
+            "angle_of_view",
+            "lat",
+            "lon",
+        ]
+        # Keep only those columns that exist
+        export_df = export_df[[col for col in ordered_columns if col in export_df.columns]]
+
+        return dcc.send_data_frame(export_df.to_csv, "export.csv", index=False), translate("download_ready", lang)
 
     except Exception as e:
-        return None, str(e), False
+        return None, str(e)
 
 
 @app.callback(Output("export-title", "children"), Input("language", "data"))
